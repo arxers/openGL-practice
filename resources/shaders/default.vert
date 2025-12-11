fc9c26b5f924fb1fa884eda4792d772b80b3283e
jason_home <arxers@gmail.com>
2025-12-12 06:46:56 +0800

    added textures, todo refactor into texture class

--- resources/shaders/default.vert
+++ resources/shaders/default.vert
@@ -1,15 +1,25 @@
 #version 330 core
+// Positions/Coordinates
 layout (location = 0) in vec3 aPos;
+// Color
 layout (location = 1) in vec3 aColor;
+// Texture
+layout (location = 2) in vec2 aTex;
 
 // Outputs color for the fragment shader
 out vec3 color;
 
+// Output texture for fragment shader
+out vec2 texCoord;
+
+// Controls scale of vertices
 uniform float scale;
 
 void main()
 {
+    // Outputs the positions/coordinates of all vertices
     gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z, 1.0);
     // Assigns the colors from the Vertex data to "color"
     color = aColor;
+    texCoord = aTex;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 #include <cmath>
 
 #include "../inc/Shader.hpp"
@@ -10,24 +11,38 @@
 
 using std::sqrt;
 
+// GLfloat vertices[] =
+// {   //          COORDINATES                     /       COLORS
+//     -0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f,     // Lower left corner
+//      0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f,     // Lower right corner
+//      0.0f,   0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   1.0f, 0.6f,  0.32f,     // Upper corner
+//     -0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f, 0.45f, 0.17f,     // Inner left
+//      0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f, 0.45f, 0.17f,     // Inner right
+//      0.0f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f      // Inner down
+//     };   
+
+// GLuint indices[] = {
+//     0, 3, 5, // Lower left triangle
+//     3, 2, 4, // Lower right triangle
+//     5, 4, 1  // Upper triangle
+// };
+
+GLfloat vertices[] =
+{   //          COORDINATES                     /       COLORS
+    -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
+    -0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,   0.0f, 1.0f,
+     0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
+     0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,   1.0f, 0.0f
+};
+
+GLuint indices[] = {
+    0, 2, 1, // Lower left triangle
+    0, 3, 2, // Lower right triangle
+};
+
+
 int main()
 {
-    // Vertices coordinates
-    GLfloat vertices[] =
-    {   //          COORDINATES                     /       COLORS
-        -0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f,     // Lower left corner
-         0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f,     // Lower right corner
-         0.0f,   0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   1.0f, 0.6f,  0.32f,     // Upper corner
-        -0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f, 0.45f, 0.17f,     // Inner left
-         0.25f,  0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f, 0.45f, 0.17f,     // Inner right
-         0.0f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f      // Inner down
-        };   
-
-    GLuint indices[] = {
-        0, 3, 5, // Lower left triangle
-        3, 2, 4, // Lower right triangle
-        5, 4, 1  // Upper triangle
-    };
 
     // Init GLFW
     glfwInit();
@@ -57,20 +72,51 @@ int main()
     // params: x_begin, y_begin, x_end, y_end
     glViewport(0, 0, 800, 800);
 
-    Shader shaderProgram("shaders/default.vert", "shaders/default.frag");
+    Shader shaderProgram("resources/shaders/default.vert", "resources/shaders/default.frag");
 
     VAO VAO1;
     VAO1.Bind();
 
     VBO VBO1(vertices, sizeof(vertices));
     EBO EBO1(indices, sizeof(indices));
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
 
+    // Gets ID of uniform called "scale"
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+    // Texture
+    int widthImg, heightImg, numColCh;
+    stbi_set_flip_vertically_on_load(1);
+    unsigned char* bytes = stbi_load("resources/textures/popcat.jpg", &widthImg, &heightImg, &numColCh, 0);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    // glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(bytes);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    shaderProgram.Activate();
+    glUniform1i(tex0Uni, 0);
 
     // Main loop
     while (!glfwWindowShouldClose(window))
@@ -82,11 +128,12 @@ int main()
         // Tell OpenGL which Shader Program we want to use
         shaderProgram.Activate();
         // Assigns a value to the uniform; NOTE: Must be done after shaderProgram.activate()
-        glUniform1f(uniID, -0.9f);
+        glUniform1f(uniID, 0.0f);
+        glBindTexture(GL_TEXTURE_2D, texture);
         // Bind the VAO so OpenGL knows to use it
         VAO1.Bind();
         // Draw the triangle using the GL_TRIANGLES primitive
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // Swap the back buffer with front buffer
         glfwSwapBuffers(window);
 
@@ -99,6 +146,7 @@ int main()
     VBO1.Delete();
     EBO1.Delete();
     shaderProgram.Delete();
+    glDeleteTextures(1, &texture);
 
     // Delete window before ending the program
     glfwDestroyWindow(window);
