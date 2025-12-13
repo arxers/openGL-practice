48c956170e0bf1accb8a2a4aac5c8c1f9274a5a1
arxers <arxers@gmail.com>
2025-12-14 04:06:44 +0800

    WIP camera class

---
+++ src/Camera.cpp
@@ -0,0 +1,63 @@
+#include "../inc/Camera.hpp"
+
+Camera::Camera(int width, int height, glm::vec3 position)
+{
+    width = width;
+    height = height;
+    Position = position;
+}
+
+void Camera::Matrix(float FOVdeg, float nearPlane, float farPlane, Shader &shader, const char *uniform)
+{
+    glm::mat4 view = glm::mat4(1.0f);
+    glm::mat4 proj = glm::mat4(1.0f);
+
+    view = glm::lookAt(Position, Position + Orientation, Up);
+    proj = glm::perspective(glm::radians(FOVdeg), (float)(width / height), nearPlane, farPlane);
+
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, uniform), 1, GL_FALSE, glm::value_ptr(proj * view));
+}
+void Camera::Inputs(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        Position += speed + Orientation;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        Position += speed + -glm::normalize(glm::cross(Orientation, Up));
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        Position += speed + -Orientation;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        Position += speed + glm::normalize(glm::cross(Orientation, Up));
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        Position += speed + Up;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS) {
+        Position += speed + -Up;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+        speed = 0.4f;
+    }
+    else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE) {
+        speed = 0.1f;
+    }
+
+    if (glfwGetKey(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+        double mouseX, mouseY;
+        glfwGetCursorPos(window, &mouseX, &mouseY);
+
+        float rotX = sensitivity * (float)(mouseY, - (height / 2)) / height;
+        float rotY = sensitivity * (float)(mouseX, - (height / 2)) / height;
+
+        glm::vec3 newOrientation = glm::rotate(Orientation, glm::radians(-rotX), glm::normalize(glm::cross(Orientation, Up)));
+        if (!((glm::angle(newOrientation, Up) <= glm::radians(5.0f)) || (glm::angle(newOrientation, -Up) <= glm::radians(5.0f)))) {
+            Orientation = newOrientation;
+        }
+        Orientation = glm::rotate(Orientation, glm::radians(-rotY), Up);
+        glfwSetCursorPos(window, width / 2, height / 2);
+    }
+}
